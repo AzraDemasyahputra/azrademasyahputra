add9de8a5b0b5da60b4abe641c8e5a25bfa3651e
AzraDemasyahputra <115337669+AzraDemasyahputra@users.noreply.github.com>
2022-10-08 13:00:22 +0700

    Add files via upload

---
+++ tugas python azra.py
@@ -0,0 +1,22 @@
+#tugas python
+#membuat a string
+#dengan single Quotes
+Juang1 = 'puisi azra'
+print("mimpi adalah kunci")
+print(Juang1)
+#membuat String
+#dengan double Quotes
+Juang2 = "untuk kita menaklukan dunia"
+print("\nberlarilah tanpa lelah ")
+print(Juang2)
+#membuat String
+#dengan tiga Quotes
+Juang3 = '''sampai engkau meraihnya"laskar pelangi takkan terikat waktu"'''
+print("\nbebaskan mimpi diangkasa :")
+print(Juang3)
+#Creating String with triple
+#Quotes allows multiple lines
+Juang4 = '''gapai
+         mimpimu'''
+print("\nharapanku pada mimpiku")
+print(Juang4)
